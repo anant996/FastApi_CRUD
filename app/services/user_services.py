90696bfac03e490c39ddccabedbaac1dabf7389a
anant996 <anant.chaudhary@dataverze.ai>
2024-03-07 14:43:07 +0530

    initial commit

--- app/services/user_services.py
+++ app/services/user_services.py
@@ -1,11 +1,15 @@
 from fastapi import HTTPException, status
 from app.utils.dummy_db import demo_db
+from pydantic import EmailStr
 
-async def create_user(user_id:int, name:str):
-    if user_id in demo_db:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists")
+async def create_user(user_id:int, name:str, email:EmailStr):
+    # if user_id in demo_db:
+    #     raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User already exists")
+    for user in demo_db.values():
+        if user['email'] == email:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already exists")
     else:
-        demo_db[user_id] = {"id":user_id, "name": name}
+        demo_db[user_id] = {"id":user_id, "name": name, "email":email}
         return {"message": "User created successfully", "user": demo_db[user_id]}, status.HTTP_201_CREATED
 
 async def get_user(user_id:int):
@@ -14,9 +18,14 @@ async def get_user(user_id:int):
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     
-async def update_user(user_id:int, name:str):
+async def update_user(user_id:int, name:str, email: EmailStr):
     if user_id in demo_db:
-        demo_db[user_id]["name"] = name
+        for user in demo_db.values():
+            if user['email'] == email:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already exists")
+            else:
+                demo_db[user_id]['name'] = name
+                demo_db[user_id]['email'] = email 
         return {"message": "User updated successfully", "user": demo_db[user_id]}, status.HTTP_200_OK
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
@@ -33,4 +42,15 @@ async def all_users():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No users found")
     return demo_db, status.HTTP_200_OK
 
+async def get_users_by_name(name:str):
+    matches = [user for user in demo_db.values() if name in user['name']]
+    if not matches:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No users found with names containing 'nt'")
+    return matches, status.HTTP_200_OK
 
+async def get_user_by_nameemail(name:str, email:EmailStr):
+    match = [user for user in demo_db.values() if user['name'] == name and user['email'] == email]
+    if not match:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No users found with given name and email")
+    else:
+        return match, status.HTTP_200_OK
